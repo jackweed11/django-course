cddabf09d35ca4a72b745f89b280e6e7060842fe
Jeff <jeff.engelmeyer@gmail.com>
2016-09-21 12:00:17 -0400

    added some more files

--- bookstore/bookstore/urls.py
+++ bookstore/bookstore/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'bookstore.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', 'store.views.index', name='index'),
+    url(r'^store/', 'store.views.store', name='store'),
     url(r'^admin/', include(admin.site.urls)),
 ]
